d4f7b8a1d09a2642c9a791e82711e0190aa5adcb
Coding Tiger <coding.tiger.mail@gmail.com>
2024-12-27 17:16:58 +0530

    useState Hook

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,58 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 const About = () => {
-  return <div>About page</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+    // https://www.backend.com/api/signup
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <form>
+      <h1>Register !</h1>
+      <input
+        type="text"
+        placeholder="Enter Name here..."
+        autoComplete="name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <input
+        type="email"
+        placeholder="Enter Email here..."
+        autoComplete="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <div className="pass">
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="Enter pasword here..."
+          autoComplete="current-password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <div className="icon">
+          {showPassword ? (
+            <FaEye size={28} onClick={() => setShowPassword(false)} />
+          ) : (
+            <FaEyeSlash size={28} onClick={() => setShowPassword(true)} />
+          )}
+        </div>
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
+    </form>
+  );
 };
 
 export default About;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  // useState Hook
+  const [count, setCount] = useState(0);
+  //     variable - function          initial value
+
+  const handleMinus = () => {
+    setCount((pre) => pre - 1);
+  };
+
+  const handlePlus = () => {
+    setCount((pre) => pre + 1);
+  };
+
+  return (
+    <div className="home">
+      <div className="temp">
+        <h2 onClick={handleMinus}> - </h2>
+        <h1>{count}</h1>
+        <h2 onClick={handlePlus}> + </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
