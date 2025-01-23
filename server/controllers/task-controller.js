e0eced9ecdfedc65e65cb53369be8f22fcbcf25b
Coding Tiger <coding.tiger.mail@gmail.com>
2025-01-23 16:59:09 +0530

    CRUD

--- server/controllers/task-controller.js
+++ server/controllers/task-controller.js
@@ -11,5 +11,23 @@ exports.addTask = async (req, res) => {
   res.json({ msg: "Task is added !", newTask });
 };
 
+exports.getAllTasks = async (req, res) => {
+  const data = await Task.find().sort({ createdAt: -1 }).limit(3).skip(2);
+  res.status(200).json({ msg: "All tasks fetched !", data });
+};
+
+exports.getSingleTask = async (req, res) => {
+  const { id } = req.params;
+
+  const task = await Task.findById(id);
+  res.status(200).json({ msg: "Single task fetched !", task });
+};
+
+exports.deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  await Task.findByIdAndDelete(id);
+  res.status(200).json({ msg: "task Deleted !" });
+};
 
 // CRUD - Create , Read , Update , Delete
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { addTask } = require("./controllers/task-controller");
+const {
+  addTask,
+  getAllTasks,
+  getSingleTask,
+  deleteTask,
+} = require("./controllers/task-controller");
 
 const router = express.Router();
 
 router.post(`/task`, addTask);
+router.get(`/task`, getAllTasks);
+router.get(`/task/:id`, getSingleTask);
+router.delete(`/task/:id`, deleteTask);
 
 module.exports = router;
